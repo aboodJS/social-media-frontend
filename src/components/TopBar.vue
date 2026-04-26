90332dfde1dbef1914391d9ef67377147ef766fe
ajs <140010746+aboodJS@users.noreply.github.com>
2026-04-26 21:52:42 +0300

    made the skeleton for the landing page

---
+++ src/components/TopBar.vue
@@ -0,0 +1,6 @@
+<template>
+    <nav class="flex gap-8 justify-center w-screen sticky top-0 bg-transparent">
+        <div class="font-bold text-[#4648b8] text-lg cursor-pointer">Feed</div>
+        <div class="font-bold text-[#4648b8] text-lg cursor-pointer">About</div>
+    </nav>
+</template>
