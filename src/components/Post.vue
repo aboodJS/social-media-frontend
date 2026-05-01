4e739776c1fafcf978d69e3e3f0d1ca1f86209ea
ajs <140010746+aboodJS@users.noreply.github.com>
2026-05-01 22:21:56 +0300

    added a post component

---
+++ src/components/Post.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps(["title", "body", "score"])
+</script>
+
+<template>
+    <div class="border border-amber-100 rounded-lg grid w-4xl h-fit py-5">
+        <h1 class="text-white text-2xl font-bold justify-self-center text-left">{{ title }}</h1>
+        <p class="text-white justify-self-center text-left">{{ body }}</p>
+        <p class="text-white justify-self-center text-left">{{ score }}</p>
+        
+    </div>
+</template>
--- src/pages/Feed.vue
+++ src/pages/Feed.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+import { onBeforeMount, ref } from 'vue';
 import FooterBar from '@/components/FooterBar.vue';
+import Post from '@/components/Post.vue';
 import TopBar from '@/components/TopBar.vue';
+const posts = ref()
+
+
+onBeforeMount(async () => {
+    
+    posts.value = await fetch("http://localhost:3000/posts").then(j => j.json()).then(r => r)
+
+})
 
 </script>
 
 <template>
     <TopBar></TopBar>
-    <main class="h-screen"></main>
+    <main class="h-screen grid gap-3 content-start justify-center">
+        <Post v-for="post in posts" :title="post.title" :body="post.body" :score="post.score"></Post>
+    </main>
     <FooterBar></FooterBar>
 </template>
