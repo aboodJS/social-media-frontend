3f5ecac828bba4ff450f70f8ff75ac51a24fc24b
ajs <140010746+aboodJS@users.noreply.github.com>
2026-05-03 21:43:17 +0300

    the browser now recives and saves the token in session storage

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -6,10 +6,11 @@ const props = defineProps(["path", "function"])
 const name = ref("")
 const password = ref("")
 
-async function sendData(loginName, loginPassword) {
-    await fetch(props.path, {method: "POST", body:{name: loginName, password: loginPassword}}).then(j => j.json()).then(r => console.log(r))
-    console.log(loginName)
-    console.log(loginPassword)
+async function sendData() {
+    const request = await fetch(props.path, {method: "POST",headers: {"Content-type": "application/json"}, body:JSON.stringify({name: name.value, password: password.value})})
+    const data = await request.json()
+    sessionStorage.setItem("token", data.token)
+    console.log(sessionStorage.getItem("token"))
     
 }
 
@@ -19,7 +20,7 @@ async function sendData(loginName, loginPassword) {
     <div>
         <input  v-model="name" class="rounded-lg outline-0 p-2 border-3 border-[#4649b8]" placeholder="username" type="text">
         <input  v-model="password" class="rounded-lg outline-0 p-2 border-3 border-[#4649b8]" placeholder="password" type="password">
-        <button @click="() => sendData(name, password)">{{ props.function }}</button>
+        <button @click="() => sendData()">{{ props.function }}</button>
     </div>
     
 </template>
