a22fc6c8210e09c8a32654712609e4902b0c53ad
ajs <140010746+aboodJS@users.noreply.github.com>
2026-05-09 22:59:15 +0300

    will redo the fetching method

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -5,18 +5,19 @@ const props = defineProps(["path", "function"])
 
 const name = ref("")
 const password = ref("")
+const errStat = ref(false)
+const errVal = ref()
 
 async function sendData() {
-        const request = await fetch(props.path, {method: "POST",headers: {"Content-type": "application/json"}, body:JSON.stringify({name: name.value, password: password.value}), credentials: 'include' })
-        const data = await request.json()
-        sessionStorage.setItem("token", data.token)
-        await cookieStore.set("Set-Cookie", data.cookie)
-        location.reload()    
+   
 }
 
 </script>
 
 <template>
+    <div v-if="errStat" class="bg-amber-800 text-white">
+        {{ errVal }}
+    </div>
     <div>
         <input  v-model="name" class="rounded-lg outline-0 p-2 border-3 border-[#4649b8]" placeholder="username" type="text">
         <input  v-model="password" class="rounded-lg outline-0 p-2 border-3 border-[#4649b8]" placeholder="password" type="password">
